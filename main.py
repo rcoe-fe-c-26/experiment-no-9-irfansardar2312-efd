08e19a34c42b8cd817f90bc3a53290f89287d57e
irfansardar2312-efd <irfansardar2312@gmail.com>
2026-03-02 07:58:23 +0530

    Implement factorial calculation in main.py
    Added functionality to compute the factorial of a given integer.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-# AIM: Design a Python program to compute 
-# the factorial of a given integer N.
-# Coder:
-# Date:
+# AIM: Design a Python program to compute the factorial of a given integer N.
+# Coder: Irfan Sardar
+# Date: 30-01-26
 
 print("--- Factorial Finder ---\n")
+num = int(input("Enter Number: "))
 
-
-# Write your code here
+if num < 0:
+    print(f"Factorial of {num} is Not Defined")
+else:
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"Factorial of {num} is {factorial}")
